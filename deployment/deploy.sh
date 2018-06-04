bf81ef9a798050add9fe5802d4b0a5d0dec4df95
TOGM <36015720+TOGM@users.noreply.github.com>
2018-06-04 08:18:15 -0500

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,7 +2,8 @@ sudo apt-get -y install python3-pip
 pip install --upgrade pip
 python3 -m pip install -U discord.py
 python3 -m pip install -U requests
-git clone https://github.com/humankite/discord-poolbot.git poolbot
+cd ~
+git clone https://github.com/togm/masari-bot.git masaribot
 sudo cp -a ~/poolbot/deployment/masaribot.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable poolbot
