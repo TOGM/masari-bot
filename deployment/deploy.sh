76c6dd28d787ec9f82adae5c249163418fc2ec50
TOGM <36015720+TOGM@users.noreply.github.com>
2018-06-04 08:26:57 -0500

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,7 +4,7 @@ python3 -m pip install -U discord.py
 python3 -m pip install -U requests
 cd ~
 git clone https://github.com/togm/masari-bot.git masaribot
-sudo cp -a ~/poolbot/deployment/masaribot.service /lib/systemd/system
+sudo cp -a ~/masaribot/deployment/masaribot.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable masaribot
 sudo systemctl start masaribot
